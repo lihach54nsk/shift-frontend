5230e653602d18cae4be1a299d396d6718520853
Luzyanin Alexey <261-96@mail.ru>
2019-07-08 14:44:25 +0700

    bullshit

--- create_Exist_Travel.js
+++ create_Exist_Travel.js
@@ -9,9 +9,13 @@ function Create_List_Of_existing_Travels() {
 
         newDiv.innerHTML = "<p class=\"travel_name\">Paris</p>\n" +
             "                <p class=\"travel_name\">Date</p>\n" +
-            "                <button class=\"editTravel_" + i.toString() + "\" value=\""+ i.toString() + "\">Редактировать</button>\n";
+            "                <button type=\"submit\" class=\"editTravel_" + i.toString() + "\" value=\""+ i.toString() + "\" onclick=\"Open_Exist_Travel(" + i.toString() + ")\">Редактировать</button>\n";
         findDiv[0].appendChild(newDiv);
     }
+}
+
+function Open_Exist_Travel(value) {
+    document.location = "exist_Travel.html?value=" + value.toString();
 }
 
 function Create_Exist_Places() {
@@ -46,4 +50,33 @@ function Create_Exist_Places() {
             "                    </div>"
         findElement[0].appendChild(newElement);
     }
+}
+
+function CreateNewNodeOfExistTravels(){
+    var findNewElement = document.getElementsByClassName('list');
+    countPlace++;
+    var newListElement = document.createElement('li');
+    newListElement.className = "list_elem_" + countPlace.toString();
+    newListElement.innerHTML = "<div  class=\"destination_" + countPlace.toString() + " point\">\n" +
+        "                        <div class=\"in\"><p>Место отправления: <input type=\"text\" name=\"from_" + countPlace.toString() + "\" class=\"from\"></p></div>\n" +
+        "                        <div class=\"in\"><p>Место прибытия: <input type=\"text\" name=\"to_" + countPlace.toString() + "\" class=\"to\"></p></div>\n" +
+        "                        <div class=\"in\"><p>Дата отправления: <input type=\"datetime-local\" name=\"dateFrom_" + countPlace.toString() + "\" class=\"dateFrom\"></p></div>\n" +
+        "                        <div class=\"in\"><p>Дата прибытия: <input type=\"datetime-local\" name=\"dateTo_" + countPlace.toString() + "\" class=\"dateTo\"></p></div>\n" +
+        "                        <div class=\"in\"><p>Расстояние до пункта назначения: <input type=\"text\" name=\"distance_" + countPlace.toString() + "\" class=\"distance\"></p></div>\n" +
+        "                        <div class=\"in\"><p>Выберите вид транспорта: <select name=\"transfer\" class=\"transfer_" + countPlace.toString() + "\">\n" +
+        "                            <option value=\"auto\">Автомобиль</option>\n" +
+        "                            <option value=\"society\">Общественный транспорт</option>\n" +
+        "                            <option value=\"air\">Самолёт</option>\n" +
+        "                            <option value=\"train\">Поезд</option>\n" +
+        "                            <option value=\"walk\">Пешком</option>\n" +
+        "                            <option value=\"other\">Другое</option>\n" +
+        "                        </select></p></div>\n" +
+        "                        <div class=\"in\"><p>Затраты на транспорт: <input type=\"text\" name=\"transferCost_"+ countPlace.toString() + "\" class=\"transferCost\"></p></div>\n" +
+        "                        <div class=\"noteFrom_" + countPlace.toString() + " note\"><p>Заметки к месту: <textarea></textarea></p></div>\n" +
+        "                    </div>"
+    findNewElement[0].appendChild(newListElement);
+    //var newDiv=document.createElement('div');
+    //newDiv.className="new";
+    //findElem=document.getElementsByClassName("list_elem_" + countList.toString());
+    //findElem[0].appendChild(newDiv);
 }
