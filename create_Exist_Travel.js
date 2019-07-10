0afa30ce42a8db2140defbcf2f9d048ffeeb6788
Luzyanin Alexey <261-96@mail.ru>
2019-07-10 17:48:30 +0700

    done with GET (done with showing of exist travel)

--- create_Exist_Travel.js
+++ create_Exist_Travel.js
@@ -1,28 +1,35 @@
 var countTravel = 3;
 var countPlace = 1;
 
+function GetJason(jason) {
+   return JSON.parse(jason);
+}
+
 function Create_List_Of_existing_Travels() {
+    //let ret = createRequest();
     let xhrTravelsList = new XMLHttpRequest();
-    xhrTravelsList.open('GET', 'URL.URL', true); // GetTravels()
-    xhrTravelsList.setRequestHeader('UserId', 'UserA');
+    xhrTravelsList.open('GET', '/api/v001/travels', true); // GetTravels()
+    xhrTravelsList.onreadystatechange = function () {
+        if (xhrTravelsList.readyState === XMLHttpRequest.DONE && xhrTravelsList.status === 200) {
+            alert('DONE GetTravel');
+            let JSON = GetJason(xhrTravelsList.responseText);
+            alert(JSON.toString());
+            countTravel = JSON.length;
+            alert(countTravel);
+
+            for(i = 1; i <= countTravel; i++) {
+                let findDiv = document.getElementsByClassName('travel_list');
+                let newDiv = document.createElement('div');
+                newDiv.className = 'exist_travels travel_' + i.toString();
+
+                newDiv.innerHTML = "<p class=\"travel_name\">" + JSON[i - 1].name + "</p>\n" + // xhrTravelsList.responseText.nameOfTravel
+                    "               <button type=\"submit\" class=\"editTravel_" + i.toString() + "\" value=\""+ i.toString() + "\" onclick=\"Open_Exist_Travel(" + i.toString() + ")\">Редактировать</button>\n";
+                findDiv[0].appendChild(newDiv);
+            }
+        }
+    }
+    xhrTravelsList.setRequestHeader('userId', 'UserA');
     xhrTravelsList.send();
-
-    if (xhrTravelsList.status !== 200) {
-        alert('ERROR');
-    } else {
-        countTravel = xhrTravelsList.responseText;
-    }
-
-    for(i = 1; i <= countTravel; i++) {
-        let findDiv = document.getElementsByClassName('travel_list');
-        let newDiv = document.createElement('div');
-        newDiv.className = 'exist_travels travel_' + i.toString();
-
-        newDiv.innerHTML = "<p class=\"travel_name\">Paris</p>\n" + // xhrTravelsList.responseText.nameOfTravel
-            "                <p class=\"travel_name\">Date</p>\n" + // xhrTravelsList.responseText.DateOfTravel
-            "                <button type=\"submit\" class=\"editTravel_" + i.toString() + "\" value=\""+ i.toString() + "\" onclick=\"Open_Exist_Travel(" + i.toString() + ")\">Редактировать</button>\n";
-        findDiv[0].appendChild(newDiv);
-    }
 }
 
 function Open_Exist_Travel(value) {
@@ -31,62 +38,88 @@ function Open_Exist_Travel(value) {
 
 function Create_Exist_Places() {
     //countPlace++;
-    var travelId = 'получить из value на странице '; // travelId, нужен будет при отправке данных
-    /*function OnLoad() {
-            var paramValue = window.location.href.split("?")[1].split("=")[1];
-            document.getElementById("UsName").innerHTML = paramValue;
-        }
-    */
+    let travelId = window.location.href.split("?")[1].split("=")[1];
+
     let xhrTravels = new XMLHttpRequest(); // CHECK TO WORK - GetTravel()
-    xhrTravels.open('GET', 'URL.URL', false);
+    xhrTravels.open('GET', '/api/v001/travels/1', false);
     xhrTravels.setRequestHeader('userId','UserA');
     xhrTravels.send();
 
-    if (xhrTravels.status !== 200) {
-        alert('ERROR!');
-    } else {
-        countPlace = xhrTravels.responseText;
-    }
+    let JSONTravelName = GetJason(xhrTravels.responseText);
+    document.querySelectorAll('div.name_of_Travel p input')[0].value = JSONTravelName.name;
+
     /*запрос и обработка количества мест
     * countPlace = n;
     * не забыть присвоить название путешествия в форме названия бэд-трипа
     * */
 
     let xhrTravelInfo = new XMLHttpRequest(); // GetMoves()
-    xhrTravelInfo.open('GET', 'URL.URL', false);
-
+    xhrTravelInfo.open('GET', '/api/v001/moves?travelId=' + travelId.toString(), false);
+    xhrTravelInfo.setRequestHeader('userId','UserA');
     xhrTravelInfo.send();
 
-    let xhrMoveNotes = new XMLHttpRequest();// GetMovesNotes()
-    xhrMoveNotes.open('GET','URL.URL', false);
-    xhrMoveNotes.setRequestHeader(userId, 'UserA');
-    xhrMoveNotes.send();
-    /*запросить информацию о путешествии(-ях???)*/
+    let JSONTravelInfo = GetJason(xhrTravelInfo.responseText);
+    alert(JSONTravelInfo);
+    countPlace = JSONTravelInfo.length;
+    let arrId = []; // MoveId
+    let arrTransferId = [];
+    let arrFromPlace = [];
+    let arrFromDate = [];
+    let arrToPlace = [];
+    let arrToDate = [];
+    let arrDistance = [];
+    let arrMoney = [];
 
-    let xhrTravelNotes = new XMLHttpRequest(); //GetTravelNotes()
-    xhrTravelNotes.open('GET', 'URL.URL', false);
-    xhrTravelNotes.setRequestHeader(userId, 'UserA');
+    for (let j = 0; j < countPlace;j++) {
+        arrId.push(JSONTravelInfo[j].id);
+        arrTransferId.push(JSONTravelInfo[j].transferId);
+        arrFromPlace.push(JSONTravelInfo[j].fromPlace);
+        arrFromDate.push(new Date(JSONTravelInfo[j].fromDate));
+        arrToPlace.push(JSONTravelInfo[j].toPlace);
+        arrToDate.push(new Date(JSONTravelInfo[j].toDate));
+        arrDistance.push(JSONTravelInfo[j].distance);
+        arrMoney.push(JSONTravelInfo[j].money);
+    }
+    alert(arrFromDate);
+    let arrMoveNotes = [];
+
+    for(let g = 0; g < countPlace; g++) {
+        let xhrMoveNotes = new XMLHttpRequest();// GetMoveNotes()
+        xhrMoveNotes.open('GET', '/api/v001/notes/moves?moveId=' + arrId[g].toString(), false);
+        xhrMoveNotes.setRequestHeader('userId', 'UserA');
+        xhrMoveNotes.send();
+        alert('Request sent');
+        let JSONMoveNotes = GetJason(xhrMoveNotes.responseText);
+        if (JSONMoveNotes == ''){
+            arrMoveNotes.push(" ");
+        } else {
+            arrMoveNotes.push(JSONMoveNotes[0].text);
+        }
+        /*запросить информацию о путешествии(-ях???)*/
+    }
+
+    alert(arrMoveNotes);
+
+    let xhrTravelNotes = new XMLHttpRequest(); // GetTravelNotes()
+    xhrTravelNotes.open('GET', '/api/v001/notes/travels?travelId=' + travelId.toString(), false);
+    xhrTravelNotes.setRequestHeader('userId', 'UserA');
     xhrTravelNotes.send();
 
-    if(xhrTravelNotes.status !== 200) {
-        alert('ERROR');
-    } else {
-        let travelNote = xhrTravelNotes.responseText; // заметка на всё путешествие
-    }
+    let TravelNote = GetJason(xhrTravelNotes.responseText)[0].text;
+    alert(TravelNote);
     /*запросить информацию о заметках по путешествию*/
 
-    for(i = 1; i <= countPlace; i++) {
-
+    for(let i = 0; i < countPlace; i++) {
         var findElement = document.getElementsByClassName('list');
         var newElement = document.createElement('li');
-        newElement.className = "list_elem_" + i.toString();
-        newElement.innerHTML = "<div class=\"destination_" + i.toString() + " point\">\n" +
-            "                        <div class=\"in\"><p>Место отправления: <input type=\"text\" name=\"from_" + i.toString() + "\" class=\"from\"></p></div>\n" +
-            "                        <div class=\"in\"><p>Место прибытия: <input type=\"text\" name=\"to_" + i.toString() + "\" class=\"to\"></p></div>\n" +
-            "                        <div class=\"in\"><p>Дата отправления: <input type=\"datetime-local\" name=\"dateFrom_" + i.toString() + "\" class=\"dateFrom\"></p></div>\n" +
-            "                        <div class=\"in\"><p>Дата прибытия: <input type=\"datetime-local\" name=\"dateTo_" + i.toString() + "\" class=\"dateTo\"></p></div>\n" +
-            "                        <div class=\"in\"><p>Расстояние до пункта назначения: <input type=\"text\" name=\"distance_" + i.toString() + "\" class=\"distance\"></p></div>\n" +
-            "                        <div class=\"in\"><p>Выберите вид транспорта: <select name=\"transfer\" class=\"transfer_" + i.toString() + "\">\n" +
+        newElement.className = "list_elem_" + (i + 1).toString();
+        newElement.innerHTML = "<div class=\"destination_" + (i + 1).toString() + " point\">\n" +
+            "                        <div class=\"in\"><p>Место отправления: <input type=\"text\" value=\"" + arrFromPlace[i].toString() + "\" name=\"from_" + (i + 1).toString() + "\" class=\"from\"></p></div>\n" +
+            "                        <div class=\"in\"><p>Место прибытия: <input type=\"text\" value=\"" + arrToPlace[i].toString() + "\" name=\"to_" + (i + 1).toString() + "\" class=\"to\"></p></div>\n" +
+            "                        <div class=\"in\"><p>Дата отправления: <input value=\"" + arrFromDate[i] + "\" type=\"datetime-local\" name=\"dateFrom_" + (i + 1).toString() + "\" class=\"dateFrom\"></p></div>\n" +
+            "                        <div class=\"in\"><p>Дата прибытия: <input value=\"" + arrToDate[i] + "\" type=\"datetime-local\" name=\"dateTo_" + (i + 1).toString() + "\" class=\"dateTo\"></p></div>\n" +
+            "                        <div class=\"in\"><p>Расстояние до пункта назначения: <input value=\"" + arrDistance[i].toString() + "\" type=\"text\" name=\"distance_" + (i + 1).toString() + "\" class=\"distance\"></p></div>\n" +
+            "                        <div class=\"in\"><p>Выберите вид транспорта: <select name=\"transfer\" class=\"transfer_" + (i + 1).toString() + "\">\n" +
             "                            <option value=\"auto\">Автомобиль</option>\n" +
             "                            <option value=\"society\">Общественный транспорт</option>\n" +
             "                            <option value=\"air\">Самолёт</option>\n" +
@@ -94,11 +127,12 @@ function Create_Exist_Places() {
             "                            <option value=\"walk\">Пешком</option>\n" +
             "                            <option value=\"other\">Другое</option>\n" +
             "                        </select></p></div>\n" +
-            "                        <div class=\"in\"><p>Затраты на транспорт: <input type=\"text\" name=\"transferCost_" + i.toString() + "\" class=\"transferCost\"></p></div>\n" +
-            "                        <div class=\"noteFrom_" + i.toString() + " note\"><p>Заметки к месту: <textarea></textarea></p></div>\n" +
+            "                        <div class=\"in\"><p>Затраты на транспорт: <input value=\"" + arrMoney[i].toString() + "\" type=\"text\" name=\"transferCost_" + (i + 1).toString() + "\" class=\"transferCost\"></p></div>\n" +
+            "                        <div class=\"noteFrom_" + (i + 1).toString() + " note\"><p>Заметки к месту: <textarea>" + arrMoveNotes[i].toString() + "</textarea></p></div>\n" +
             "                    </div>"
         findElement[0].appendChild(newElement);
     }
+    document.querySelectorAll('div.note_Travel p textarea')[0].value = TravelNote;
 }
 
 function CreateNewNodeOfExistTravels(){
